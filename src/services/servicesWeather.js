46c6263663d3e9da0eded5a939430fa83f9f3f2c
Alonso García <globulitorojo5@gmail.com>
2023-03-12 16:41:51 -0600

    Agrego cositas

---
+++ src/services/servicesWeather.js
@@ -0,0 +1,43 @@
+const FETCH_OPTIONS = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key":
+            "b75b1e2baamsh544e9e21543498dp10333djsn563f8586f059",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+};
+
+export async function getWeatherFrom(query = 'Mexico') {
+    const response = await fetch(
+        `https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`,
+        FETCH_OPTIONS
+    )
+
+    const data = await response.json();
+    const { location, current } = data;
+    const { country, localTime, name } = location;
+    const {
+        condition,
+        humidity,
+        feelslike_c,
+        temp_c,
+        is_day,
+        wind_kph,
+        wind_dir,
+    } = current;
+    const { code, text } = condition;
+
+    return {
+        conditionCode: code,
+        ConditionText: text,
+        country,
+        localTime,
+        name,
+        humidity,
+        isDay: is_day,
+        feelsLike: feelslike_c,
+        temperature: temp_c,
+        windSpeed: wind_kph,
+        windDir: wind_dir
+    }
+}
